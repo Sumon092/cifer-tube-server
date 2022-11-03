d3e38a42afdb01d11e7e8a7822736881ace68c36
SUMON <m.sumon@outlook.com>
2022-11-04 00:20:32 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors())
+const port = process.env.PORT || 5000;
+
+
+
+
+
+app.listen(port,()=>{
+    console.log('listening to port',port);
+})
