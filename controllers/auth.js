90e3085bc5fb80e38f4b02132760d1ba905cbd44
SUMON <m.sumon@outlook.com>
2022-11-04 14:17:52 +0600

    global error handler adde

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = require("../models/User.js")
 // console.log(User);
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     try {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
         const newUser = new User({ ...req.body, password: hash });
         await newUser.save();
         res.status(200).send('user added successfully');
-    } catch (error) {
-        //error
+    } catch (err) {
+        next(err)
     }
 }
 module.exports = {
